9a12c4c0ec91514c62ea23fe2e1d9a1618e52dee
Arthur-St <artrexwow@gmail.com>
2020-08-05 23:47:23 -0300

    Fix
    fixed: animation of several wall jumps in sequence
    fixed: walking / running on ice

--- Plataforma-AZ/Assets/Scripts/Move/MoveVelocity.cs
+++ Plataforma-AZ/Assets/Scripts/Move/MoveVelocity.cs
@@ -60,27 +60,38 @@ public class MoveVelocity : MonoBehaviour, IMove
     {
         //speed = Mathf.Clamp(velocityVector.x + speed, -GetComponent<PlayerController>().speed, GetComponent<PlayerController>().speed);
         rb2D.velocity = new Vector2(GetComponent<PlayerController>().speed * velocityVector.x / 2, rb2D.velocity.y);
+        EcoMoveReset(8);
     }
     private void WallToFlip()
     {
         rb2D.velocity = new Vector2(velocityVector.x * GetComponent<PlayerController>().speed * 1.5f, rb2D.velocity.y);
+        EcoMoveReset(8);
     }
     private void IceMove() //teste
     {
-        speed = Mathf.Clamp(velocityVector.x + speed, -GetComponent<PlayerController>().speed, GetComponent<PlayerController>().speed);
-        rb2D.velocity = new Vector2(speed, rb2D.velocity.y);
-        EcoMoveReset();
+        if (PlayerController.isWallEdge)
+        {
+            rb2D.velocity = new Vector2(velocityVector.x * GetComponent<PlayerController>().speed, rb2D.velocity.y);
+            speed = 0;
+        }
+        else
+        {
+            speed = Mathf.Clamp(velocityVector.x + speed, -GetComponent<PlayerController>().speed, GetComponent<PlayerController>().speed);
+            rb2D.velocity = new Vector2(speed, rb2D.velocity.y);
+        }
+        EcoMoveReset(2);
+
     }
-    private void EcoMoveReset()
+    private void EcoMoveReset(float multiply)
     {
         if (speed >= 0.1f)
         {
-            speed -= 0.02f;
+            speed -= 0.02f * multiply;
         }
         else
         if (speed <= -0.01f)
         {
-            speed += 0.02f;
+            speed += 0.02f * multiply;
         }
     }
 }
