61853e6d79d6a8460a63f0b17e078b611e7eb536
Arthur-St <artrexwow@gmail.com>
2020-10-21 18:24:40 -0300

    Add - Parallax
    Add Parallax

---
+++ Plataforma-AZ/Assets/Scripts/Scenario/NewParallax.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewParallax : MonoBehaviour
+{
+    public GameObject cam;
+    private float length, startpos;
+    public float parallaxEffect;
+
+    private void Awake()
+    {
+        cam = GameObject.FindGameObjectWithTag("MainCamera");
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float dist = (cam.transform.position.x * parallaxEffect);
+
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+
+        if (temp > startpos + length /2) 
+            startpos += length;
+        else if (temp < startpos - length /2) 
+            startpos -= length;
+
+    }
+}
