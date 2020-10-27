04fd9d58b3bc42b787ce32fa481fd6c42280e306
Arthur-St <artrexwow@gmail.com>
2020-10-27 16:36:22 -0300

    Add - CloundMaker
    Add:
    Cloud Maker prefab
    CloudSpawn Script

--- Plataforma-AZ/Assets/Scripts/State Pattern/CloundSpawn.cs
+++ Plataforma-AZ/Assets/Scripts/State Pattern/CloundSpawn.cs
@@ -4,18 +4,53 @@ using UnityEngine;
 
 public class CloundSpawn : MonoBehaviour
 {
-    private List<GameObject> clound;
-    public float selfDestructionTime;
-    public GameObject cloundPrefab;
+    public Transform cloudStartSpawn;
+    public Transform cloudEndSpawn;
+    public float timeSpawnCD;
+    public float rangeSpawn;
+    public float cloudMinSpeed;
+    public float cloudMaxSpeed;
+    public int maxCloudCreated;
+    public GameObject cloudPreFab;
+    public Sprite[] cloudSprite;
+    public List<GameObject> cloudInstance;
+    public bool cloudCD;
+
+
 
     void Start()
     {
-        
+        cloudInstance.Add(Instantiate(cloudPreFab, cloudStartSpawn.position + new Vector3(0, UnityEngine.Random.Range(-rangeSpawn, rangeSpawn), 0), transform.rotation));
+        //cloundCD = true;
+
     }
 
     void Update()
     {
-        GameObject cloundInstance = Instantiate(cloundPrefab, transform.position, transform.rotation);
-        
+        if (!cloudCD && cloudInstance.Count < maxCloudCreated)
+        {
+            cloudCD = true;
+            StartCoroutine(SpawnCloud());
+        }
+        else if (cloudInstance[0].transform.position.x >= cloudEndSpawn.position.x)
+        {
+            Destroy(cloudInstance[0].gameObject);
+            cloudInstance.RemoveAt(0);
+        }
+        for (int i = 0; i < cloudInstance.Count; i++)
+        {
+            cloudInstance[i].transform.position = Vector3.MoveTowards(cloudInstance[i].transform.position, new Vector3(cloudEndSpawn.position.x, cloudInstance[i].transform.position.y), UnityEngine.Random.Range(cloudMinSpeed, cloudMaxSpeed) * Time.deltaTime);
+        }
+    }
+
+    private IEnumerator SpawnCloud()
+    {
+        yield return new WaitForSecondsRealtime(timeSpawnCD);
+        cloudInstance.Add(Instantiate(cloudPreFab, cloudStartSpawn.position + new Vector3(0,UnityEngine.Random.Range(-rangeSpawn,rangeSpawn),0), transform.rotation));
+        int index = cloudInstance.Count - 1;
+        cloudInstance[index].GetComponent<SpriteRenderer>().sprite = cloudSprite[UnityEngine.Random.Range(0,cloudSprite.Length)];
+        Debug.Log(cloudInstance.Count);
+        cloudCD = false;
+
     }
 }
